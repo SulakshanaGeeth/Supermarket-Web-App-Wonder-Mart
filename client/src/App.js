b9bcd6121fdbeb0d3424ce665e72a1033f44c3ae
IT20244170-Wijegunarathna <it20244170@my.sliit.lk>
2022-09-14 12:24:19 +0530

    modify Index.js file.

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,4 @@
-
-
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
@@ -10,48 +8,33 @@ import Login from "./components/UserManagement/Login";
 import EditProfile from "./components/UserManagement/EditProfile";
 
 //Product managemet Imports
-
-    import Header from "./components/Header"
-    import Home from "./components/Home";
-    import AddProduct from "./components/AddProduct";
-    import About from "./components/About"
-    import Products from "./components/Product/Products"
-    import ProductDetail from "./components/Product/ProductDetail";
+// import Header from "./components/Header";
+// import Home from "./components/Home";
+import AddProduct from "./components/AddProduct";
+import About from "./components/About";
+import Products from "./components/Product/Products";
+import ProductDetail from "./components/Product/ProductDetail";
 
 function App() {
   return (
     <div className="App">
       <ToastContainer />
-      <Router>
-        <Routes>
-            <header>
-                <Header/>
-            </header>
 
-            <main>
-                <Routes>
-                    <Route path="/" element={<Home/> } exact />
-                    <Route path="/add" element={<AddProduct/> } exact />
-                    <Route path="/products" element={<Products/> } exact />
-                    <Route path="/about" element={<About/> } exact />
-                    <Route path="/products/:id" element={<ProductDetail/> } exact />
-                </Routes>
-            </main>
+      <Routes>
+        {/* <Route path="/" element={<Home />} exact /> */}
+        <Route path="/add" element={<AddProduct />} exact />
+        <Route path="/products" element={<Products />} exact />
+        <Route path="/about" element={<About />} exact />
+        <Route path="/products/:id" element={<ProductDetail />} exact />
 
+        <Route path="/register" element={<Register />} />
 
+        <Route path="/profile" element={<EditProfile />} />
 
-
-
-          <Route path="/register" element={<Register />} />
-
-          <Route path="/profile" element={<EditProfile />} />
-
-          <Route path="/" element={<Login />} />
-        </Routes>
-      </Router>
+        <Route path="/" element={<Login />} />
+      </Routes>
     </div>
   );
-
 }
 
 export default App;
