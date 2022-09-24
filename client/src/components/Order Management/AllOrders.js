e105974496cfa4e79ce9e617d656508117e73bb7
Janindu Sachinthana <106908984+Janindu98@users.noreply.github.com>
2022-09-24 22:28:52 +0530

    Update All Orders page

--- client/src/components/Order Management/AllOrders.js
+++ client/src/components/Order Management/AllOrders.js
@@ -1,11 +1,82 @@
 import React from 'react';
+import axios from 'axios';
+
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Button  from '@mui/material/Button';
 
 export default class AllOrders extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            Orders: []
+        }
+    }
+
+    async componentDidMount() {
+        await axios.get('http://localhost:8070/order')
+        .then( (response) => {
+            this.setState({Orders:response.data})
+            console.log(response.data)
+        })
+        .catch((err) => console.log(err.message))
+    }
 
     render() {
         return (
             <div>
-                <h1>AllOrders</h1>
+                
+                <TableContainer sx={{ width:"1000px", height:"auto", marginLeft:"25px", marginTop:"25px"}} >
+                    <Table sx={{width:"1000px"}} stickyHeader aria-label="sticky table" >
+                        <TableHead>
+                            <TableRow>
+                                <TableCell sx={{backgroundColor:'#b0bec5', textAlign: 'center'}} > Order ID </TableCell>
+                                <TableCell sx={{backgroundColor:'#b0bec5', textAlign: 'center'}} > Name </TableCell>
+                                <TableCell sx={{backgroundColor:'#b0bec5', textAlign: 'center'}} > Address </TableCell>
+                                <TableCell sx={{backgroundColor:'#b0bec5', textAlign: 'center'}} > Mobile </TableCell>
+                                <TableCell sx={{backgroundColor:'#b0bec5', textAlign: 'center'}} > Amount (LKR) </TableCell>
+                            </TableRow>
+                        </TableHead>
+            
+                        <TableBody>
+                            {
+                                this.state.Orders.map((order) => (
+                                    <TableRow hover >
+                                        <TableCell> {order._id} </TableCell>
+                                        <TableCell> {order.Name} </TableCell>
+                                        <TableCell> {order.Address} </TableCell>
+                                        <TableCell> {order.Mobile} </TableCell>
+                                        <TableCell sx={{textAlign: 'right',paddingRight:"50px"}} > {order.Amount}.00 </TableCell>
+                                        <TableCell> <Button variant='outlined' color='primary'sx={{backgroundColor:'#03a9f4', color:'black'}} > View Details </Button> </TableCell>
+                                    </TableRow>
+                                ))
+                            }                            
+
+                            {/* <TableRow hover >
+                                <TableCell> 632ed3680d068eba89bb97c4 </TableCell>
+                                <TableCell> Janindu Sachinthana </TableCell>
+                                <TableCell> Kandana </TableCell>
+                                <TableCell> 774138745 </TableCell>
+                                <TableCell> 1178.00 </TableCell>
+                            </TableRow>
+
+                            <TableRow hover >
+                                <TableCell> 632ed3680d068eba89bb97c4 </TableCell>
+                                <TableCell> Janindu Sachinthana </TableCell>
+                                <TableCell> Kandana </TableCell>
+                                <TableCell> 774138745 </TableCell>
+                                <TableCell> 1178.00 </TableCell>
+                            </TableRow> */}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+
             </div>
         )
     }
