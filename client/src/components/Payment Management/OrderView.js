ccf40a2e505924a6aceda1a181656ecd15b1163a
Janindu Sachinthana <106908984+Janindu98@users.noreply.github.com>
2022-09-25 11:44:56 +0530

    Modify to download invoice pdf in order view page

--- client/src/components/Payment Management/OrderView.js
+++ client/src/components/Payment Management/OrderView.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useParams } from "react-router-dom";
+import JsPDF from "jspdf";
 
 import logo from "./../../Images/Wonder mart Logo 2.jpg";
 import Header from '../HeaderFooter/Header';
@@ -15,6 +16,7 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 
 
 const OrderView = () => {
@@ -46,80 +48,101 @@ const OrderView = () => {
 
     }, []);
 
+    const DownloadInvoice = () => {
+        const report = new JsPDF("portrait", "px", "a1");
+        report.html(document.querySelector("#invoice")).then(() => {
+        report.save("invoice.pdf");
+        })
+    }
+
     return (
         <div>
             <Header />
 
-            <img alt="WMlogo" src={logo} style={{width:"100px", height:"100px", marginTop:"25px", marginLeft:"60%",position:"absolute"}} />
-            <Typography variant="h4" sx={{position:"absolute", marginTop:"8%", marginLeft:"57%"}} > Wonder Mart </Typography> 
-            <Typography variant="h1" sx={{position:"relative", marginTop:"1%", marginLeft:"17%"}} > Invoice </Typography>
+            <div id="invoice">
 
-            <Typography variant="h5" sx={{position:"relative", marginTop:"1%", marginLeft:"17%"}} > Client Details </Typography>
+                <img alt="WMlogo" src={logo} style={{width:"100px", height:"100px", marginTop:"25px", marginLeft:"60%",position:"absolute"}} />
+                <Typography variant="h4" sx={{position:"absolute", marginTop:"8%", marginLeft:"57%"}} > Wonder Mart </Typography> 
+                <Typography variant="h1" sx={{position:"relative", marginTop:"1%", marginLeft:"17%"}} > Invoice </Typography>
 
-            <Typography variant="h6" sx={{position:"relative", marginTop:"1%", marginLeft:"17%"}} > Name    : {Name} </Typography>
-            <Typography variant="h6" sx={{position:"relative", marginTop:"0%", marginLeft:"17%"}} > Address : {Address} </Typography>
-            <Typography variant="h6" sx={{position:"relative", marginTop:"0%", marginLeft:"17%"}} > Email   : {Email} </Typography>
-            <Typography variant="h6" sx={{position:"relative", marginTop:"0%", marginLeft:"17%"}} > Mobile  : {Mobile} </Typography>
+                <Typography variant="h5" sx={{position:"relative", marginTop:"1%", marginLeft:"17%"}} > Client Details </Typography>
 
-            <Box sx={{width:"350px", position:"absolute", marginTop:"-12%", marginLeft:"54%", outline:"none"}}>
-                <Typography variant="h5" sx={{position:"relative", marginTop:"10%", marginLeft:"0%"}} > Order Details </Typography>
+                <Typography variant="h6" sx={{position:"relative", marginTop:"1%", marginLeft:"17%"}} > Name    : {Name} </Typography>
+                <Typography variant="h6" sx={{position:"relative", marginTop:"0%", marginLeft:"17%"}} > Address : {Address} </Typography>
+                <Typography variant="h6" sx={{position:"relative", marginTop:"0%", marginLeft:"17%"}} > Email   : {Email} </Typography>
+                <Typography variant="h6" sx={{position:"relative", marginTop:"0%", marginLeft:"17%"}} > Mobile  : {Mobile} </Typography>
 
-                <Typography variant="h6" sx={{position:"relative", marginTop:"2%", marginLeft:"0%"}} > Order No    : {OrderID} </Typography>
-                <Typography variant="h6" sx={{position:"relative", marginTop:"0%", marginLeft:"0%"}} > Order Date : {Address} </Typography>
-                {/* <Typography variant="h6" sx={{position:"relative", marginTop:"0%", marginLeft:"0%"}} > </Typography>
-                <Typography variant="h6" sx={{position:"relative", marginTop:"0%", marginLeft:"0%"}} > </Typography> */}
-            </Box>            
+                <Box sx={{width:"350px", position:"absolute", marginTop:"-12%", marginLeft:"54%", outline:"none"}}>
+                    <Typography variant="h5" sx={{position:"relative", marginTop:"10%", marginLeft:"0%"}} > Order Details </Typography>
 
-            <TableContainer sx={{ width:'1000px', marginLeft:'250px', marginTop:'10px' }} component={Paper}>
-                <Table sx={{ width:'1000px'}} aria-label="customized table">
-                    <TableHead>
-                        <TableRow>
-                            <TableCell sx={{textAlign: 'center', fontWeight:'bold', fontSize:'17px'}} >Product Name</TableCell>
-                            <TableCell sx={{ width:'100px', textAlign: 'center',fontWeight:'bold', fontSize:'17px'}}>Quantity</TableCell>
-                            <TableCell sx={{ width:'150px',textAlign: 'center', fontWeight:'bold', fontSize:'17px'}}>Product Price</TableCell>
-                            <TableCell sx={{ width:'150px',textAlign: 'center', fontWeight:'bold', fontSize:'17px'}}>Amount</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {
-                            Products.map((item) => (
-                                <TableRow>
-                                    <TableCell sx={{}} > {item.ProductName} </TableCell>
-                                    <TableCell sx={{ width:'100px', textAlign: 'center'}}> {item.Quantity} </TableCell>
-                                    <TableCell sx={{ width:'150px',textAlign: 'right',paddingRight:"50px"}}> {item.ProductPrice} .00</TableCell>
-                                    <TableCell sx={{ width:'150px',textAlign: 'right',paddingRight:"100px"}}>{(item.Quantity*item.ProductPrice)}.00</TableCell>
-                                </TableRow>
-                            ))
-                        }
-                        
-                        {/* <TableRow>
-                            <TableCell sx={{}} >Raigam Soy Devilled Chicken Flavor 110g</TableCell>
-                            <TableCell sx={{ width:'100px', textAlign: 'center'}}>12</TableCell>
-                            <TableCell sx={{ width:'150px',textAlign: 'right',paddingRight:"50px"}}>2500.00</TableCell>
-                            <TableCell sx={{ width:'150px',textAlign: 'right',paddingRight:"100px"}}>5000.00</TableCell>
-                        </TableRow> */}
+                    <Typography variant="h6" sx={{position:"relative", marginTop:"2%", marginLeft:"0%"}} > Order No    : {OrderID} </Typography>
+                    <Typography variant="h6" sx={{position:"relative", marginTop:"0%", marginLeft:"0%"}} > Order Date : {Address} </Typography>
+                    {/* <Typography variant="h6" sx={{position:"relative", marginTop:"0%", marginLeft:"0%"}} > </Typography>
+                    <Typography variant="h6" sx={{position:"relative", marginTop:"0%", marginLeft:"0%"}} > </Typography> */}
+                </Box>            
 
-                        <TableRow>
-                            <TableCell colSpan={3} sx={{textAlign: 'right',fontWeight:'bold',fontSize:'17px',paddingRight:"225px"}} >Sub Total</TableCell>                            
-                            <TableCell sx={{ width:'150px',textAlign: 'right',fontWeight:'bold',paddingRight:"100px"}}>{Amount}.00</TableCell>
-                        </TableRow>
-                        <TableRow>
-                            <TableCell colSpan={3} sx={{textAlign: 'right',fontWeight:'bold',fontSize:'17px',paddingRight:"225px"}} >Delivery Charge</TableCell>                            
-                            <TableCell sx={{ width:'150px',textAlign: 'right',fontWeight:'bold',paddingRight:"100px"}}>200.00</TableCell>
-                        </TableRow>
-                        <TableRow>
-                            <TableCell colSpan={3} sx={{textAlign: 'right',fontWeight:'bold',fontSize:'17px',paddingRight:"225px"}} >Total Amount</TableCell>                            
-                            <TableCell sx={{ width:'150px',textAlign: 'right',fontWeight:'bold',paddingRight:"100px"}}>{(Amount+200)}.00</TableCell>
-                        </TableRow>
-                        
-                    </TableBody>
-                </Table>
-                </TableContainer>
+                <TableContainer sx={{ width:'1000px', marginLeft:'250px', marginTop:'10px' }} component={Paper}>
+                    <Table sx={{ width:'1000px'}} aria-label="customized table">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell sx={{textAlign: 'center', fontWeight:'bold', fontSize:'17px'}} >Product Name</TableCell>
+                                <TableCell sx={{ width:'100px', textAlign: 'center',fontWeight:'bold', fontSize:'17px'}}>Quantity</TableCell>
+                                <TableCell sx={{ width:'150px',textAlign: 'center', fontWeight:'bold', fontSize:'17px'}}>Product Price</TableCell>
+                                <TableCell sx={{ width:'150px',textAlign: 'center', fontWeight:'bold', fontSize:'17px'}}>Amount</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {
+                                Products.map((item) => (
+                                    <TableRow>
+                                        <TableCell sx={{}} > {item.ProductName} </TableCell>
+                                        <TableCell sx={{ width:'100px', textAlign: 'center'}}> {item.Quantity} </TableCell>
+                                        <TableCell sx={{ width:'150px',textAlign: 'right',paddingRight:"50px"}}> {item.ProductPrice} .00</TableCell>
+                                        <TableCell sx={{ width:'150px',textAlign: 'right',paddingRight:"100px"}}>{(item.Quantity*item.ProductPrice)}.00</TableCell>
+                                    </TableRow>
+                                ))
+                            }
+                            
+                            {/* <TableRow>
+                                <TableCell sx={{}} >Raigam Soy Devilled Chicken Flavor 110g</TableCell>
+                                <TableCell sx={{ width:'100px', textAlign: 'center'}}>12</TableCell>
+                                <TableCell sx={{ width:'150px',textAlign: 'right',paddingRight:"50px"}}>2500.00</TableCell>
+                                <TableCell sx={{ width:'150px',textAlign: 'right',paddingRight:"100px"}}>5000.00</TableCell>
+                            </TableRow> */}
 
-                <Typography variant="h4" sx={{marginLeft: '17%', marginTop: '2%'}} > Thank You for your business !</Typography>
-                <Typography variant="h6" sx={{marginLeft: '17%'}} > If you have any question about this invoice Please contact us.</Typography>
-                <Typography variant="body1" sx={{marginLeft: '17%'}} > Email : wondermart@gmail.com </Typography>
-                <Typography variant="body1" sx={{marginLeft: '17%'}} > Mobile : 0714578658/0778745852 </Typography>
+                            <TableRow>
+                                <TableCell colSpan={3} sx={{textAlign: 'right',fontWeight:'bold',fontSize:'17px',paddingRight:"225px"}} >Sub Total</TableCell>                            
+                                <TableCell sx={{ width:'150px',textAlign: 'right',fontWeight:'bold',paddingRight:"100px"}}>{Amount}.00</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell colSpan={3} sx={{textAlign: 'right',fontWeight:'bold',fontSize:'17px',paddingRight:"225px"}} >Delivery Charge</TableCell>                            
+                                <TableCell sx={{ width:'150px',textAlign: 'right',fontWeight:'bold',paddingRight:"100px"}}>200.00</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell colSpan={3} sx={{textAlign: 'right',fontWeight:'bold',fontSize:'17px',paddingRight:"225px"}} >Total Amount</TableCell>                            
+                                <TableCell sx={{ width:'150px',textAlign: 'right',fontWeight:'bold',paddingRight:"100px"}}>{(Amount+200)}.00</TableCell>
+                            </TableRow>
+                            
+                        </TableBody>
+                    </Table>
+                    </TableContainer>
+
+                    <Typography variant="h4" sx={{marginLeft: '17%', marginTop: '2%'}} > Thank You for your business !</Typography>
+                    <Typography variant="h6" sx={{marginLeft: '17%'}} > If you have any question about this invoice Please contact us.</Typography>
+                    <Typography variant="body1" sx={{marginLeft: '17%'}} > Email : wondermart@gmail.com </Typography>
+                    <Typography variant="body1" sx={{marginLeft: '17%'}} > Mobile : 0714578658/0778745852 </Typography>
+                </div>
+                <Button variant="contained" 
+                        color="warning" 
+                        onClick={() => DownloadInvoice()}
+                        sx={{
+                            marginLeft: '70%', 
+                            marginTop:'2%',
+                            backgroundColor:"#00e676",
+                            color:"black",
+                            width:"150px"
+                        }}> Download 
+                </Button>
 
             <Footer/>
         </div>
