60a1378e0ec505992d6f9ba56dfedd643dfdeaf9
Janindu Sachinthana <106908984+Janindu98@users.noreply.github.com>
2022-09-25 12:15:13 +0530

    Add alert page and implement

---
+++ client/src/components/Order Management/Alert/Alert.js
@@ -0,0 +1,15 @@
+import Swal from "sweetalert2";
+
+export const RiderAssign = (icon, title, text) => {
+    Swal.fire({
+        icon: icon,
+        title: title,
+        text: text,
+        confirmButtonText:"OK"
+    })
+    .then((ok) => {
+        if (ok) {
+          window.location = '#'
+        }
+      });
+  };
